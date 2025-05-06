1493f6a6cd62e7fcc645c5234de6bc0ddf04294b
BenoitMain <benoitmaingon@gmail.com>
2025-05-06 16:03:02 +0200

    fix: rework

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-print(
-    f"{''.join(chr(i) for i in range(97, 123) if i not in (101,113))}", end=""
-      )
+for i in range(97, 123):
+   if i not in (101, 113):
+    print("{}".format(chr(i)), end="")
