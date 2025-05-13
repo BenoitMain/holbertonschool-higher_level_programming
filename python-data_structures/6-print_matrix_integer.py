2d174e9b958cb69c848119e1d368ee8a82c2e76d
BenoitMain <benoitmaingon@gmail.com>
2025-05-13 08:54:43 +0200

    refactor(python-data_structures): missing whitespace around operator

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for idx in range(len(row)):
             value = row[idx]
-            if idx == len(row) -1:
+            if idx == len(row) - 1:
                 print("{:d}".format(value), end="")
             else:
                 print("{:d}".format(value), end=" ")
