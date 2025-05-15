e24385827b32078587988de11f241fb5aebc2980
BenoitMain <benoitmaingon@gmail.com>
2025-05-15 12:01:31 +0200

    fix: Missing blank lines after docstring

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,6 +4,8 @@
 Adds two integers or floating-point numbers. If one of the arguments is not an
 int or a float, the function raises a TypeError with the appropriate message.
 """
+
+
 def add_integer(a, b=98):
     """
     Adds two integers or floating-point numbers.
