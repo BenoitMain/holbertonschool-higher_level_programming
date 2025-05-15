29128ca0e5367d6951abd2811e8e80f6745e027c
BenoitMain <benoitmaingon@gmail.com>
2025-05-15 15:35:14 +0200

    fix: pycodestyle

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
+
 """
 Module 2-matrix_divided
-Divides each element of a matrix (a list of lists of numbers) by div and returns
+Divides each element of a matrix (a list of lists of numbers) by div and return
 a new matrix. Floats are rounded to 2 decimal places.
 """
 
+
 def matrix_divided(matrix, div):
+
     """
     Divides all elements of matrix by div, rounding to 2 decimals.
     """
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
     for row in matrix:
         if not isinstance(row, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
     row_length = len(matrix[0])
     for row in matrix:
         if len(row) != row_length:
@@ -22,7 +26,8 @@ def matrix_divided(matrix, div):
     for row in matrix:
         for elem in row:
             if not isinstance(elem, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists)"
+                                "ofintegers/floats")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
@@ -34,4 +39,3 @@ def matrix_divided(matrix, div):
             new_row.append(round(elem / div, 2))
         new_matrix.append(new_row)
     return new_matrix
-
