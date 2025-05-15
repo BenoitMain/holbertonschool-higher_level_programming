7c84af70a18f11440144b317c65a08af0b899201
BenoitMain <benoitmaingon@gmail.com>
2025-05-15 16:48:10 +0200

    fix: Add doctyp

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 
+"""
+Module 4-print_square
+Prints a square made of the character '#'. size must be an integer greater than
+or equal to 0,otherwise an exception is raised.
+"""
+
+
 def print_square(size):
+    """
+    Prints a square of the given size using the '#' character.
+    """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
