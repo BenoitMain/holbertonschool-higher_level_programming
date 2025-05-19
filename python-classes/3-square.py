114a8ceb0d466deba45f768b475a17c26d670f01
BenoitMain <benoitmaingon@gmail.com>
2025-05-19 14:36:16 +0200

    style: Deleted extra psace

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -23,9 +23,8 @@ class Square:
         """
 
         if not isinstance(size, int):
-
             raise TypeError("size must be an integer")
-        elif size < 0 :
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
@@ -36,4 +35,4 @@ class Square:
         Returns:
             int: The current square’s area, computed as size squared.
         """
-        return self.__size  ** 2
+        return self.__size ** 2
