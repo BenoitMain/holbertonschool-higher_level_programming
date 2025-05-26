9640a421de3571eda6255cc18c365628b1445cc1
BenoitMain <benoitmaingon@gmail.com>
2025-05-26 16:20:37 +0200

    docs: Add documentation for docstring

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Module that returns the list of available attributes and methods of an object.
+"""
+
+
 class MyList(list):
     """Custom list class that extends the built-in list.
 
