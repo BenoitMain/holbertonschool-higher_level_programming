bd133cc0ffd1535be40cfe3118013edbf8e36407
BenoitMain <benoitmaingon@gmail.com>
2025-05-28 09:59:29 +0200

    refactor: change is_kind_of_class to is_same_class to ensure exact type check

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,19 +5,7 @@ Module that returns the list of available attributes and methods of an object.
 
 
 def is_same_class(obj, a_class):
-    """Check if an object is an instance of a given class.
-
-    This function returns True if `obj` is an instance of `a_class` (including subclasses),
-    and False otherwise.
-
-    Args:
-        obj (Any): The object to check.
-        a_class (type): The class to compare against.
-
-    Returns:
-        bool: True if `obj` is an instance of `a_class`, False otherwise.
+    """returns True if the obj is exactly an instance of the specified class;
+    otherwise False.
     """
-    if not isinstance(obj, a_class):
-        return False
-    else:
-        return True
+    return type(obj) is a_class
