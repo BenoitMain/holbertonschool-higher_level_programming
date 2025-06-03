1378da24c11954e0b25cd26821f30e71856f02ed
BenoitMain <benoitmaingon@gmail.com>
2025-06-03 16:55:58 +0200

    docs: Adjust docstring

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,14 +3,9 @@
 Add command-line arguments to a JSON list stored in a file.
 
 This script loads a list from `add_item.json` (creating an empty list if it
-does not exist), appends any arguments passed via the command line to that list,
-and then saves the updated list back to `add_item.json` using JSON format.
-
-Usage:
-    ./7-add_item.py [item1] [item2] [...]
-
-If `add_item.json` does not exist,  is created with an empty list before adding
-any items. Subsequent runs will preserve existing items and append new ones.
+does not exist), appends any arguments passed via the command line
+to that list, and then saves the updated list back to `add_item.json`
+using JSON format.
 
 Args:
     sys.argv[1:] (list of str): Items to add to the JSON list. Each element is
@@ -18,7 +13,10 @@ Args:
 
 
 """
+
+
 import sys
+
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
