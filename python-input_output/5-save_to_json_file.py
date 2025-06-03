a22c701a331a28ed43a9b6e2e22e4ebd9670ae05
BenoitMain <benoitmaingon@gmail.com>
2025-06-03 12:16:06 +0200

    docs(5-save_to_json_file): add Google-style docstring to save_to_json_file function

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import json
+
+def save_to_json_file(my_obj, filename):
+    """Write an object to a file in JSON format.
+
+    This function serializes the given Python object to JSON and writes
+    it to the specified file. If the file already exists, its content
+    is overwritten. If the object is not JSON-serializable, a TypeError
+    will be raised.
+
+    Args:
+        my_obj (Any): The Python object to serialize (e.g., list, dict).
+        filename (str): The path to the text file where JSON will be saved.
+
+    Returns:
+        None
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.dump(my_obj, f)
