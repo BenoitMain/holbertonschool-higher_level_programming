9f2f30d5cdc506e56081fa3788915569088cb6dd
BenoitMain <benoitmaingon@gmail.com>
2025-06-26 15:42:16 +0200

    feat(script): add 1-filter_states.py to filter states starting with 'N' from MySQL database

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Filter states starting with 'N' from MySQL database.
+
+This script connects to a MySQL database and retrieves all states whose names
+start with the letter 'N' (case-sensitive), ordered by ID in ascending order.
+
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        port=3306,
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
