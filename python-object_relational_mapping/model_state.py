d0601ba5d0e108b4899ddaac646f7ec3a8e5e883
BenoitMain <benoitmaingon@gmail.com>
2025-06-27 10:35:33 +0200

    feat: implement State class and Base instance for SQLAlchemy

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Defines a State class and a Base instance for SQLAlchemy
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+# Create the Base instance
+Base = declarative_base()
+
+class State(Base):
+    """State class for the states table"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
