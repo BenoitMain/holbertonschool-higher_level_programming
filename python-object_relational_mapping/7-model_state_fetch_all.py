ffeda1cfa1c499bc220f47089fab01a222da4305
BenoitMain <benoitmaingon@gmail.com>
2025-06-27 12:52:05 +0200

    fix : change indentation

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,9 +13,9 @@ if __name__ == "__main__":
             sys.argv[3]),
         pool_pre_ping=True)
 
-Session = sessionmaker(bind=engine)
-session = Session()
-states = session.query(State).order_by(State.id).all()
-for state in states:
-    print(f"{state.id}: {state.name}")
-session.close()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")
+    session.close()
