8ff057b532617eec2121d8f00598eaa97b48874d
BenoitMain <benoitmaingon@gmail.com>
2025-07-11 17:02:27 +0200

    feat: add multi_languages_loop.js to display programming languages

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// Array containing the lines to display
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Loop through each line in the array
+for (let i = 0; i < lines.length; i++) {
+    // Print each line to the console
+    console.log(lines[i]);
+}
