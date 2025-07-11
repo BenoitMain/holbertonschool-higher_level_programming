066c6f57e06042c08dc617df8b1a53965120cf96
BenoitMain <benoitmaingon@gmail.com>
2025-07-11 15:11:12 +0200

    fix: remove unnecessary comments in 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -3,12 +3,9 @@
 const args = process.argv.slice(2);
 // Check the number of arguments and display appropriate message
 if (args.length === 0) {
-    // No arguments provided
     console.log('No argument');
 } else if (args.length === 1) {
-    // Exactly one argument provided
     console.log('Argument found');
 } else {
-    // Multiple arguments provided
     console.log('Arguments found');
 }
