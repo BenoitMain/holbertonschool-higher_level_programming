b4eb6229a5378d56d2295c641f4d5eface874257
BenoitMain <benoitmaingon@gmail.com>
2025-07-11 15:10:39 +0200

    fix: remove unnecessary blank lines in 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-
 // Get command line arguments, excluding 'node' and script name
 const args = process.argv.slice(2);
-
 // Check the number of arguments and display appropriate message
 if (args.length === 0) {
     // No arguments provided
