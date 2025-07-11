cb0b269aa0a6ed4ebded99a60b7a9c6c2c761133
BenoitMain <benoitmaingon@gmail.com>
2025-07-11 16:49:45 +0200

    fix: adjust indentation for consistency in 5-to_integer.js

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -4,8 +4,8 @@
 const num = parseInt(process.argv[2]);
 // Check if the conversion resulted in a valid number
 if (isNaN(num)) {
-    // If not a number, display error message
+  // If not a number, display error message
   console.log('Not a number');
 } else {
-    // If valid number, display the integer value
+  // If valid number, display the integer value
   console.log('My number: ' + num);}
