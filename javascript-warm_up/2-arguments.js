633f477687319b7f28dbb9f3f663d9bb968be5e8
BenoitMain <benoitmaingon@gmail.com>
2025-07-11 15:12:48 +0200

    fix: remove unnecessary comments in 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-// Get command line arguments, excluding 'node' and script name
 const args = process.argv.length - 2;
-// Check the number of arguments and display appropriate message
+
 if (args.length === 0) {
     console.log('No argument');
 } else if (args.length === 1) {
